36eea9cdcafbfc1562241dfc73a1082288ee3f46
Sebastian Mleczko <fuxseb@gmail.com>
2024-01-24 22:16:58 +0100

    it works!

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <XboxSeriesXControllerESP32_asukiaaa.hpp>
+#include <ESP32Servo.h>
+
+#define SERVO_MIN 550
+#define SERVO_MAX 2350
+
+Servo servo1;
+XboxSeriesXControllerESP32_asukiaaa::Core xboxController;
+long servo_us;
+
+void setup() {
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  Serial.begin(115200);
+  servo1.attach(A4, SERVO_MIN, SERVO_MAX);
+  servo1.setPeriodHertz(50);
+  xboxController.begin();
+}
+
+void loop() {
+  xboxController.onLoop();
+    if (xboxController.isConnected()) {
+    if (xboxController.isWaitingForFirstNotification()) {
+      Serial.println("waiting for first notification");
+    } else {
+        servo_us = map(xboxController.xboxNotif.joyLHori, 0, XboxControllerNotificationParser::maxJoy, SERVO_MIN, SERVO_MAX);
+        Serial.println(String(servo_us));
+        servo1.writeMicroseconds(servo_us);
+    }
+  } else {
+    Serial.println("not connected");
+    if (xboxController.getCountFailedConnection() > 2) {
+      ESP.restart();
+    }
+  }
+}
